7582ccf74358d356ff851a51020b215b68725615
loganghy <loganfirth173@gmail.com>
2025-12-08 11:29:44 +0000

    added customisable themes with relevent projects and descriptions as well as a form sheet and a footer

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log('Its working')
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+	setTheme('light')
+}else{
+	setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot')
+
+
+for (var i=0; themeDots.length > i; i++){
+	themeDots[i].addEventListener('click', function(){
+		let mode = this.dataset.mode
+		console.log('Option clicked:', mode)
+		setTheme(mode)
+	})
+}
+
+function setTheme(mode){
+    document.body.classList.add(mode);
+    document.body.dataset.theme = mode;
+
+	localStorage.setItem('theme', mode)
+}
